2da01fe6a496f4c1008143926ce12d0be63ce688
nh5@wsl <holiavin.nikolai.nh5@gmail.com>
2024-10-08 18:14:39 +0300

    hw 2: add plots

--- 2/generated/trace.py
+++ 2/generated/trace.py
@@ -1,6 +1,7 @@
 import itertools
 import collections
 from collections import Counter
+import matplotlib.pyplot as plt
 
 def sliding_window(iterable, n):
     "Collect data into overlapping fixed-length chunks or blocks."
@@ -11,6 +12,10 @@ def sliding_window(iterable, n):
         window.append(x)
         yield tuple(window)
 
+def compute_stat_row(n_all, counter_entry):
+    pat, n = counter_entry
+    return (';'.join(x.strip() for x in pat), n, int(n / n_all * 100))
+
 def stat_window(lines, out_filename, pat_len):
     counter = Counter()
     all = 0
@@ -18,16 +23,33 @@ def stat_window(lines, out_filename, pat_len):
         counter[tuple(pat)] += 1
         all += 1
 
-    with open(out_filename, 'w') as outfile:
+    with open(f'{out_filename}_{pat_len}.csv', 'w') as outfile:
         outfile.write('pattern,N,%\n')
         for pat, n in counter.most_common()[:50]:
-            outfile.write(f'{';'.join(x.strip() for x in pat)},{n},{int(n/all*100)}\n')
+            r1, r2, r3 = compute_stat_row(all, (pat, n))
+            outfile.write(f'{r1},{r2},{r3}\n')
+
+    fig, ax = plt.subplots()
+    barxs = []
+    barys = []
+    for pat, n in counter.most_common()[:30]:
+        r1, r2, r3 = compute_stat_row(all, (pat, n))
+        barxs.append(r1)
+        barys.append(r2)
+    ax.bar(barxs, barys)
+    # ax.tick_params(axis='x', labelrotation=45)
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    ax.set_xlabel('pattern')
+    plt.tight_layout()
+    fig.savefig(f'{out_filename}_{pat_len}.png')
+    plt.close(fig)
+
     return counter
 
 with open('simple_trace.txt', 'r') as infile:
     for pat_len in range(1, 6):
         infile.seek(0)
-        stat_window(infile, f'simple_trace_stats_{pat_len}.csv', pat_len)
+        stat_window(infile, f'simple_trace_stats', pat_len)
 
 with open('use_trace.txt', 'r') as infile:
-    stat_window((x.rstrip(';\n') for x in infile), f'use_trace_stats.csv', 1)
+    stat_window((x.rstrip(';\n') for x in infile), f'use_trace_stats', 1)
